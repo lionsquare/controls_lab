66d697b4f9eb8d39b7781627d8704b3c5eee04a0
Sevy Harris <sevy.harris@gamil.com>
2020-06-23 13:52:57 -0400

    adding examples for camera and servo

---
+++ camera_tut.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import time
+import picamera
+
+camera = picamera.PiCamera()
+
+try:
+    camera.start_preview()
+    time.sleep(10)
+    camera.stop_preview()
+finally:
+    camera.close()
+
+
+
---
+++ servo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import time
+import wiringpi
+
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+
+delay_period = 0.1
+
+
